d156709ac841f2a9fd3b8d455552a7662849de0f
bilal-sagir <sagir.bilal@icloud.com>
2021-12-15 16:24:00 +0300

    hotfix2 v2

--- GitFlowExample/ViewController.swift
+++ GitFlowExample/ViewController.swift
@@ -14,22 +14,25 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-<<<<<<< HEAD
-    func hotfix(){
+
+    func hotfix()
+    {
         // hotfix
-=======
+    }
+    
     func app()
     {
         var version = 0.1
         var future1 = "future1"
     }
     
-    func feature2(){
+    func feature2()
+    {
         //new feature in progress
->>>>>>> Develop
     }
 
-    func feature3(){
+    func feature3()
+    {
         
     }
 }
